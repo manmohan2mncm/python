800416dd1035cb10277453f7b27d9252f02bcc50
manmohan2mncm <118954047+manmohan2mncm@users.noreply.github.com>
2023-02-23 19:20:00 +0530

    Create 1.py
    to take input in python

---
+++ 1.py
@@ -0,0 +1,8 @@
+#for integer type values
+a=int(input('enter the value: '))
+
+#for string(str) type
+b=input('enter the character: '))
+
+#for float(decimal) type
+c=float(input('enter the floating value: '))
