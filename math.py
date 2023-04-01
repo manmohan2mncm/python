29bd0c73699a60ea559c80f1ffdde3f5c0cd5575
M. Manmohan <118954047+manmohan2mncm@users.noreply.github.com>
2023-04-01 18:14:56 +0530

    math function

---
+++ math.py
@@ -0,0 +1,25 @@
+import math
+a=int(input("enter the value a: "))
+b=int(input("enter the value b: "))
+c=float(input("enter the flaoting value: "))
+
+d=math.pow(a,b)
+print(int(d))
+
+e=math.sqrt(a)
+print(int(e))
+
+f=math.ceil(c)
+print(f)
+
+g=math.fabs(c)
+print(g)
+
+j=math.fmod(a,b)
+print(j)
+
+h=math.factorial(a)
+print(h)
+
+k=math.frexp(a)
+print(k)
